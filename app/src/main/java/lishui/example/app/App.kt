5b2baae2dce7718c92fc9e35ea48a7e9308f4210
lishui.lin <lishui.lin@tcl.com>
2020-09-23 11:29:00 +0800

    update code

---
+++ app/src/main/java/lishui/example/app/App.kt
@@ -0,0 +1,11 @@
+package lishui.example.app
+
+import android.app.Application
+import lishui.example.app.utils.LogUtils
+
+class App: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ app/src/main/java/lishui/example/app/ui/MainActivity.kt
@@ -0,0 +1,19 @@
+package lishui.example.app.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import lishui.example.app.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
